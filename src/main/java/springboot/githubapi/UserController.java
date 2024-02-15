f4508bafee9d48a2a7ef606ccf6c1362aaac93b7
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-02-15 23:05:00 +0100

    Added error handling to api calls

--- src/main/java/springboot/githubapi/UserController.java
+++ src/main/java/springboot/githubapi/UserController.java
@@ -1,11 +1,16 @@
 package springboot.githubapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import springboot.githubapi.model.Repo;
 
 import java.util.List;
@@ -15,7 +20,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -25,8 +30,12 @@ public class UserController {
         try {
             List<Repo> userRepos = List.of(userService.getUserRepos(userName));
             return ResponseEntity.ok(userRepos);
+        } catch (WebClientResponseException e) {
+            logger.info(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), "User not found"));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+            logger.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal server error"));
         }
     }
 }
